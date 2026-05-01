af5b869fada692eab1e2dd4381dc80deaa1b1e63
Faza-SAPUTRA <fatirzaidan.p@gmail.com>
2026-05-01 14:19:09 +0700

    Add python-dotenv and load .env in app.py

--- app/app.py
+++ app/app.py
@@ -11,6 +11,9 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 if CURRENT_DIR not in sys.path:
     sys.path.insert(0, CURRENT_DIR)
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from models import db, SpopData
 import pandas as pd
 
